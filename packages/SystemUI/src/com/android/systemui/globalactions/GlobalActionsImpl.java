c1786333c1b5865dfa7b77cc8a2601c85d66ffe0
maxwen <max.weninger@gmail.com>
2017-12-10 09:30:50 +0100

    Use new gradient dialog also for recovery/bootloader action
    abc: adapted to our code
    
    original commit: https://gerrit.omnirom.org/#/c/27327/
    
    Change-Id: I3a05ca554927dacff7e10d901de5947569a08d07

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsImpl.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Typeface;
+import android.os.PowerManager;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
@@ -98,7 +99,13 @@ public class GlobalActionsImpl implements GlobalActions {
         TextView message = d.findViewById(R.id.text1);
         message.setTextColor(color);
         message.setTypeface(Typeface.create("google-sans",Typeface.NORMAL));
-        if (isReboot) message.setText(R.string.reboot_to_reset_message);
+        if (reason != null && PowerManager.REBOOT_BOOTLOADER.equals(reason)) {
+            message.setText(com.android.internal.R.string.reboot_to_bootloader_message);
+        } else if (reason != null && PowerManager.REBOOT_RECOVERY.equals(reason)) {
+            message.setText(com.android.internal.R.string.reboot_to_recovery_message);
+        } else if (isReboot) {
+            message.setText(R.string.reboot_message);
+        }
 
         Point displaySize = new Point();
         mContext.getDisplay().getRealSize(displaySize);
