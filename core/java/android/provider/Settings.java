1f2e8c718c2a5dd41f7c07e4582d61b076f9af08
Chris <chris@cypheros.co>
2018-01-01 22:45:19 -0500

    base: Support black theme
    Adds a system black theme as an optional choice
    
    Change-Id: I0d6f0c4c03ddeefb27e5d0da4ad49c9b4d4845e0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4094,6 +4094,7 @@ public final class Settings {
          * 0 - wallpaper based
          * 1 - force light
          * 2 - force dark
+         * 3 - force black
          * @hide
          */
         public static final String SYSTEM_THEME_STYLE = "system_theme_style";
